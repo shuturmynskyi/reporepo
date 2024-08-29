bd6b18627ed8a43d565fe0e77d691ea3c7cdb2db
Serhii Shuturmynskyi <shuturmynskyi.s@gmail.com>
2024-08-29 23:17:07 +0300

    Bew changes

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Hello world! 3rd time");
-        System.out.println("what is this?");
     }
 }
